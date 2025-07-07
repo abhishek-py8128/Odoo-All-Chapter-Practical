f1780d57191085ff8fb1b89796ee8bf7f55a43a7
abhishek-py8128 <abhitapaniya@gmail.com>
2025-07-07 18:21:31 +0530

    Create Compute Function And Change For Ui

--- models/real_model.py
+++ models/real_model.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import datetime
 
 class RealEstateProperty(models.Model):
     _name = 'real.estate.property'
@@ -29,12 +30,17 @@ class RealEstateProperty(models.Model):
         ('Offer Accepted', 'Offer Accepted')
     ], default='New', string='Status')
 
+    property_selection = fields.Selection(selection=[
+        ('House','House'),
+        ('Apartment','Apartment')
+    ], default='House')
+
     property_type_id = fields.Many2one("property.type", string="Property Type")
     salesperson_name = fields.Char(string="Salesperson", compute='_compute_salesperson_name', store=False)
     buyer_name = fields.Char(string="Buyer", compute='_compute_buyer_name', store=False)
 
     tag_ids = fields.Many2many("estate.property.tag", string='Property Tag')
-    offer_ids = fields.One2many('estate.property.offer','property_id',string='Property Offer')
+    offer_ids = fields.One2many('estate.property.offer','property_id')
 
     total_area = fields.Integer(string='Total Area', compute='_compute_total_area')
     best_price = fields.Integer(string='Best Price', compute='_compute_best_price')
@@ -90,7 +96,7 @@ class Estate_Property_Offer(models.Model) :
     _description = 'Property Offer'
 
     price = fields.Float(string='Price')
-    status = fields.Selection(selection=[('Accepted','Accepted'),('Refused','Refused')], string='Status', copy=False)
+    status = fields.Selection(selection=[('Accepted','Accepted'),('Refused','Refused')], string='Status', copy=False, default='Accepted')
     partner_id = fields.Many2one(
         'res.partner',
         string='Partner',
@@ -99,7 +105,13 @@ class Estate_Property_Offer(models.Model) :
     )
     property_id = fields.Many2one(
         'real.estate.property',
-        string='Property Id',
+        string='Property id',
         required=True
     )
+    validity = fields.Integer(default=7, string='Validity (days)')
+    date_deadline = fields.Date(compute='_compute_date_deadline')
 
+    @api.depends('validity','create_date')
+    def _compute_date_deadline(self):
+        for record in self :
+            pass
